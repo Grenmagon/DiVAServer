c6ba1a9c05ccbcb1312b2f7271559b752d8c9fb8
Katharina Kruspel <katharina.kruspel@stud.fh-campuswien.ac.at>
2025-05-03 17:51:41 +0200

    adapting to different screen sizes, but calendar function still dont work

---
+++ public/Login/js/login.js
@@ -0,0 +1,11 @@
+import { NewsController } from './loginController.js';
+import * as Helper from "../../js/helper.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+    Helper.loadCSS("/Login/css/login.css");
+	//const newsController = new NewsController();
+	//newsController.init();
+	//newsController.loadNews(); // Start mit aktuellen News
+});
+
+
